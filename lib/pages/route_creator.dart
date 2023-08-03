255963161d7a3ba4f59d658b62f1aaa7d90519e1
Marek Brandt <marek7119@gmail.com>
2023-08-03 22:33:12 +0200

    REG-29 - Provide reorderable list of markers

--- lib/pages/route_creator.dart
+++ lib/pages/route_creator.dart
@@ -26,44 +26,72 @@ class _RouteCreatorPageState extends State<RouteCreatorPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const AppHeader(),
-      body: FlutterMap(
-        mapController: _mapController,
-        options: MapOptions(
-          onLongPress: (tapPosition, coordinates) {
-            markers.add(Marker(
-                point: coordinates,
-                builder: (context) => const Icon(
-                      Icons.circle,
-                      color: Colors.red,
-                      size: 12,
-                    )));
-            setState(() {});
-          },
-          center: const LatLng(54.372158, 18.638306),
-          zoom: 12,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Flexible(
+              child: FlutterMap(
+                mapController: _mapController,
+                options: MapOptions(
+                  onLongPress: (tapPosition, point) {
+                    markers.add(Marker(
+                        key: Key(point.toString()),
+                        point: point,
+                        builder: (context) => const Icon(
+                              Icons.circle,
+                              color: Colors.red,
+                              size: 12,
+                            )));
+                    setState(() {});
+                  },
+                  center: const LatLng(54.372158, 18.638306),
+                  zoom: 12,
+                ),
+                nonRotatedChildren: const [
+                  RichAttributionWidget(
+                    popupInitialDisplayDuration: Duration(seconds: 5),
+                    animationConfig: ScaleRAWA(),
+                    attributions: [
+                      TextSourceAttribution(
+                        'OpenStreetMap contributors',
+                      ),
+                      TextSourceAttribution(
+                        'This attribution is the same throughout this app, except where otherwise specified',
+                        prependCopyright: false,
+                      ),
+                    ],
+                  ),
+                ],
+                children: [
+                  TileLayer(
+                    urlTemplate:
+                        'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    userAgentPackageName: 'com.regattaBuddy.app',
+                  ),
+                  MarkerLayer(markers: markers)
+                ],
+              ),
+            ),
+            Flexible(
+              child: ReorderableListView(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  children: markers
+                      .map((element) => ListTile(
+                            key: element.key,
+                            title: Text(element.point.toString()),
+                          ))
+                      .toList(),
+                  onReorder: (oldIndex, newIndex) => setState(() {
+                        if (oldIndex < newIndex) {
+                          newIndex -= 1;
+                        }
+                        final Marker item = markers.removeAt(oldIndex);
+                        markers.insert(newIndex, item);
+                      })),
+            )
+          ],
         ),
-        nonRotatedChildren: const [
-          RichAttributionWidget(
-            popupInitialDisplayDuration: Duration(seconds: 5),
-            animationConfig: ScaleRAWA(),
-            attributions: [
-              TextSourceAttribution(
-                'OpenStreetMap contributors',
-              ),
-              TextSourceAttribution(
-                'This attribution is the same throughout this app, except where otherwise specified',
-                prependCopyright: false,
-              ),
-            ],
-          ),
-        ],
-        children: [
-          TileLayer(
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            userAgentPackageName: 'com.regattaBuddy.app',
-          ),
-          MarkerLayer(markers: markers)
-        ],
       ),
     );
   }
