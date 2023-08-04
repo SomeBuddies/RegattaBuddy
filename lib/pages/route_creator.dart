6d344cf504e1c8ae5bdb163ef787679822556698
Marek Brandt <marek7119@gmail.com>
2023-08-04 23:11:54 +0200

    REG-29 - Add colors to indicate changes

--- lib/pages/route_creator.dart
+++ lib/pages/route_creator.dart
@@ -1,7 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:regatta_buddy/components/app_header.dart';
 import 'package:latlong2/latlong.dart';
+
+import '../widgets/app_header.dart';
+import '../widgets/complex_marker.dart';
 
 class RouteCreatorPage extends StatefulWidget {
   static const String route = '/route_creator';
@@ -14,7 +18,10 @@ class RouteCreatorPage extends StatefulWidget {
 
 class _RouteCreatorPageState extends State<RouteCreatorPage> {
   late final MapController _mapController;
-  List<Marker> markers = [];
+
+  // List<Marker> markers = [];
+  // List<Color> colors = [];
+  List<ComplexMarker> markers = [];
 
   @override
   void initState() {
@@ -35,32 +42,27 @@ class _RouteCreatorPageState extends State<RouteCreatorPage> {
                 mapController: _mapController,
                 options: MapOptions(
                   onLongPress: (tapPosition, point) {
-                    markers.add(Marker(
+                    Color randomColor =
+                        Color(Random().nextInt(0x55555555) + 0xaaaaaaaa)
+                            .withAlpha(0xff);
+                    Marker markerWithRandomColor = Marker(
                         key: Key(point.toString()),
                         point: point,
-                        builder: (context) => const Icon(
+                        builder: (context) => Icon(
                               Icons.circle,
-                              color: Colors.red,
+                              color: randomColor,
                               size: 12,
-                            )));
+                            ));
+                    markers
+                        .add(ComplexMarker(markerWithRandomColor, randomColor));
                     setState(() {});
                   },
                   center: const LatLng(54.372158, 18.638306),
                   zoom: 12,
                 ),
                 nonRotatedChildren: const [
-                  RichAttributionWidget(
-                    popupInitialDisplayDuration: Duration(seconds: 5),
-                    animationConfig: ScaleRAWA(),
-                    attributions: [
-                      TextSourceAttribution(
-                        'OpenStreetMap contributors',
-                      ),
-                      TextSourceAttribution(
-                        'This attribution is the same throughout this app, except where otherwise specified',
-                        prependCopyright: false,
-                      ),
-                    ],
+                  SimpleAttributionWidget(
+                    source: Text('OpenStreetMap contributors'),
                   ),
                 ],
                 children: [
@@ -69,7 +71,21 @@ class _RouteCreatorPageState extends State<RouteCreatorPage> {
                         'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                     userAgentPackageName: 'com.regattaBuddy.app',
                   ),
-                  MarkerLayer(markers: markers)
+                  PolylineLayer(
+                    polylines: [
+                      Polyline(
+                          points: markers
+                              .map(
+                                  (complexMarker) => complexMarker.marker.point)
+                              .toList(),
+                          isDotted: true,
+                          color: Colors.red,
+                          strokeWidth: 3)
+                    ],
+                  ),
+                  MarkerLayer(
+                      markers:
+                          markers.map((complex) => complex.marker).toList()),
                 ],
               ),
             ),
@@ -77,16 +93,17 @@ class _RouteCreatorPageState extends State<RouteCreatorPage> {
               child: ReorderableListView(
                   padding: const EdgeInsets.symmetric(horizontal: 40),
                   children: markers
-                      .map((element) => ListTile(
-                            key: element.key,
-                            title: Text(element.point.toString()),
+                      .map((complexMarker) => ListTile(
+                            key: complexMarker.marker.key,
+                            title: Text(complexMarker.marker.point.toString()),
+                            tileColor: complexMarker.color,
                           ))
                       .toList(),
                   onReorder: (oldIndex, newIndex) => setState(() {
-                        if (oldIndex < newIndex) {
+                    if (oldIndex < newIndex) {
                           newIndex -= 1;
                         }
-                        final Marker item = markers.removeAt(oldIndex);
+                        final ComplexMarker item = markers.removeAt(oldIndex);
                         markers.insert(newIndex, item);
                       })),
             )
---
+++ lib/widgets/complex_marker.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+import 'package:flutter_map/flutter_map.dart';
+
+class ComplexMarker {
+  final Marker marker;
+  final Color color;
+
+  ComplexMarker(this.marker, this.color);
+}
