476b0c39e0496987537cdcabbdb4beb00a0fba60
RushKappa <bartoszzylwis@gmail.com>
2023-10-20 13:42:15 +0200

    REG-36: add initialization of score doc for new team (#20)

--- lib/pages/user_regattas.dart
+++ lib/pages/user_regattas.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:regatta_buddy/pages/race/moderator/race_moderator_page.dart';
 import 'package:regatta_buddy/pages/race/participant/race_page.dart';
 import 'package:regatta_buddy/pages/race/participant/race_page_arguments.dart';
+import 'package:regatta_buddy/providers/firebase_writer_service_provider.dart';
 import 'package:regatta_buddy/utils/form_utils.dart';
 import 'package:regatta_buddy/widgets/app_header.dart';
 
-class UserRegattasPage extends StatefulWidget {
+class UserRegattasPage extends ConsumerStatefulWidget {
   const UserRegattasPage({super.key});
 
   static const String route = '/userRegattas';
 
   @override
-  State<UserRegattasPage> createState() => _UserRegattasPageState();
+  ConsumerState<UserRegattasPage> createState() => _UserRegattasPageState();
 }
 
-class _UserRegattasPageState extends State<UserRegattasPage> {
+class _UserRegattasPageState extends ConsumerState<UserRegattasPage> {
   final eventIdController = TextEditingController(text: "uniqueEventID");
   final teamController = TextEditingController(text: "teamX");
 
@@ -32,6 +34,8 @@ class _UserRegattasPageState extends State<UserRegattasPage> {
 
   @override
   Widget build(BuildContext context) {
+    final dbRef = ref.watch(firebaseWriterServiceProvider);
+
     return Scaffold(
       appBar: const AppHeader(),
       body: Center(
@@ -46,12 +50,13 @@ class _UserRegattasPageState extends State<UserRegattasPage> {
             controller: teamController,
           ),
           ElevatedButton(
-            onPressed: () => Navigator.pushNamed(
-              context,
-              RacePage.route,
-              arguments: RacePageArguments(
-                  eventIdController.text, teamController.text),
-            ),
+            onPressed: () async {
+            // TODO move team score initialization to team creation flow
+              await dbRef.initializeScoreForTeam(eventIdController.text, teamController.text);
+              Navigator.pushNamed(context, RacePage.route,
+                arguments: RacePageArguments(
+                  eventIdController.text, teamController.text));
+            },
             child: const Text('Race participant page'),
           ),
           ElevatedButton(
