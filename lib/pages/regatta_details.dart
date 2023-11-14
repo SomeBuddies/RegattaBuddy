bd408f36450f1577b7f4186d0b024c411cbf2b4b
Marek Brandt <marek7119@gmail.com>
2023-11-14 20:20:30 +0100

    REG-71 - fix use of notifiers

--- lib/pages/regatta_details.dart
+++ lib/pages/regatta_details.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:fpdart/fpdart.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:regatta_buddy/models/event.dart';
-import 'package:regatta_buddy/pages/race/moderator/race_moderator_page.dart';
-import 'package:regatta_buddy/pages/race/participant/race_page.dart';
-import 'package:regatta_buddy/pages/race/participant/race_page_arguments.dart';
-import 'package:regatta_buddy/providers/user_provider.dart';
 import 'package:regatta_buddy/widgets/app_header.dart';
 import 'package:regatta_buddy/widgets/event_details_display.dart';
 import 'package:regatta_buddy/widgets/event_teams_display.dart';
 
-import '../models/team.dart';
-import '../providers/event_details/teams_provider.dart';
+import 'package:regatta_buddy/widgets/go_to_event_button.dart';
 
 class RegattaDetailsPage extends StatelessWidget {
   const RegattaDetailsPage({super.key});
@@ -34,78 +27,5 @@ class RegattaDetailsPage extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-class GoToEventButton extends ConsumerStatefulWidget {
-  const GoToEventButton(this.event, {Key? key}) : super(key: key);
-  final Event event;
-
-  @override
-  ConsumerState<GoToEventButton> createState() => _GoToEventButtonState();
-}
-
-class _GoToEventButtonState extends ConsumerState<GoToEventButton> {
-  bool showButton = false;
-  String? teamId;
-
-  void onPressed() {
-    teamId == null
-        ? Navigator.pushNamed(context, RaceModeratorPage.route,
-            arguments: widget.event.id)
-        : Navigator.pushNamed(context, RacePage.route,
-            arguments: RacePageArguments(widget.event.id, teamId!));
-  }
-
-  Future<void> loadNeededData(Event event) async {
-    if (isEventActive(event) && isEventConnectedToUser(event)) {
-      setState(() {
-        showButton = true;
-      });
-    }
-  }
-
-  bool isEventActive(Event event) {
-    DateTime now = DateTime.now();
-    return now.isAfter(event.date.subtract(const Duration(hours: 1))) &&
-        now.isBefore(event.date.add(const Duration(days: 1)));
-  }
-
-  bool isEventConnectedToUser(Event event) {
-    final userId = ref
-        .read(currentUserDataProvider)
-        .whenOrNull(data: (userData) => userData.uid);
-
-    if (event.hostId == userId) return true;
-
-    final List<Team>? teams =
-        ref.watch(teamsProvider(event)).whenOrNull(data: (teams) => teams);
-    if (teams == null) return false;
-    var teamsWithUser = teams.filter((team) =>
-        team.members.map((teamMember) => teamMember.id).contains(userId));
-
-    if (teamsWithUser.isNotEmpty) {
-      setState(() {
-        teamId = teamsWithUser.first.id;
-      });
-      return true;
-    }
-    return false;
-  }
-
-  @override
-  void initState() {
-    loadNeededData(widget.event);
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return showButton
-        ? ElevatedButton(
-      onPressed: onPressed,
-            child: const Text("Enter event"),
-          )
-        : const SizedBox.shrink();
   }
 }
